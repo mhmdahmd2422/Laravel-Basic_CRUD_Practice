8280b0bed239360035501a54e97d2d9285a9bd41
Mohamed Ahmed <mhmdahmd2422@gmail.com>
2023-08-02 00:05:01 +0300

    Try observer to trigger mail job in the queue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Post;
+use App\Observers\PostObserver;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Post::observe(PostObserver::class);
+
         Paginator::useBootstrap();
     }
 }
